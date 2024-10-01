7e344980ff0eae2770e5ba6c1b80ba9e09371b92
Ale Giusti <104381720+devgiusti@users.noreply.github.com>
2024-09-30 21:49:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var playButton = document.getElementById('play-button');
 var pauseButton = document.getElementById('pause-button');
 var video = document.getElementById('background-video');
+var closeButton = document.getElementById('close-dialog');
 
 // Inicialmente, o botão pause-button deve estar oculto
 pauseButton.classList.add('hidden');
@@ -20,35 +21,21 @@ playButton.addEventListener('click', function() {
 
 // Evento de clique no botão de baixar foto
 pauseButton.addEventListener('click', function() {
-    // Verificar se o vídeo está tocando
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
+    // Parar o vídeo
+    video.pause();
     
-    // Mostrar novamente o botão de play
-    playButton.style.display = 'block';
+    // Mostrar a caixa de diálogo
+    var dialogBox = document.getElementById('dialog-box');
+    dialogBox.classList.remove('hidden');
 });
 
-// Evento de clique no botão de pause para ocultar e mostrar o botão de play novamente
-playButton.addEventListener('click', function() {
-    // Esconder o botão de play
-    playButton.style.display = 'none';
+// Evento de clique no botão de fechar da caixa de diálogo
+closeButton.addEventListener('click', function() {
+    // Fechar a caixa de diálogo
+    var dialogBox = document.getElementById('dialog-box');
+    dialogBox.classList.add('hidden');
     
-    // Mostrar o botão de baixar foto
-    pauseButton.classList.remove('hidden');
-});
-
-// Evento de clique no botão de pause para pausar ou continuar o vídeo e mostrar novamente o botão de play
-pauseButton.addEventListener('click', function() {
-    // Verificar se o vídeo está tocando
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-    
-    // Mostrar novamente o botão de play
+    // Retomar o vídeo e mostrar o botão de play novamente
+    video.play();
     playButton.style.display = 'block';
 });
